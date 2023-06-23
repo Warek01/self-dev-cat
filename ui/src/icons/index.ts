bb7039dab5ad9812fec3d2b34f8647b5cbd2c1ce
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2023-06-23 15:39:58 +0300

    feat: add more icons

--- ui/src/icons/index.ts
+++ ui/src/icons/index.ts
@@ -1,46 +1,55 @@
-import Bash from './Bash.tsx';
-import Css from './Css.tsx';
-import DB from './DB.tsx';
-import Discord from './Discord.tsx';
-import Docker from './Docker.tsx';
-import ExpressJs from './ExpressJs.tsx';
-import Facebook from './Facebook.tsx';
-import Figma from './Figma.tsx';
-import Git from './Git.tsx';
-import Github from './Github.tsx';
-import Gitlab from './Gitlab.tsx';
-import Globe from './Globe.tsx';
-import Hammer from './Hammer.tsx';
-import Html from './Html.tsx';
-import Instagram from './Instagram.tsx';
-import Javascript from './Javascript.tsx';
-import Jira from './Jira.tsx';
-import LinkedIn from './LinkedIn.tsx';
-import Linux from './Linux.tsx';
-import Loading from './Loading.tsx';
-import Menu from './Menu.tsx';
-import MongoDB from './MongoDB.tsx';
-import Moon from './Moon.tsx';
-import NextJs from './NextJs.tsx';
-import NodeJs from './NodeJs.tsx';
-import Npm from './Npm.tsx';
-import Pin from './Pin.tsx';
-import PostgreSql from './PostgreSql.tsx';
-import Quotes from './Quotes.tsx';
-import Redux from './Redux.tsx';
-import Sass from './Sass.tsx';
-import Spinner from './Spinner.tsx'
-import Star from './Star.tsx';
-import StencilJs from './StencilJs.tsx';
-import Sun from './Sun.tsx';
-import Tailwindcss from './Tailwindcss.tsx';
-import Typescript from './Typescript.tsx';
-import User from './User.tsx'
-import List from './List.tsx'
+import ArrowDown from './ArrowDown.tsx'
+import ArrowLeft from './ArrowLeft.tsx'
+import ArrowRight from './ArrowRight.tsx'
+import ArrowUp from './ArrowUp.tsx'
+import Bash from './Bash.tsx'
+import ChevronDown from './ChevronDown.tsx'
+import ChevronLeft from './ChevronLeft.tsx'
+import ChevronRight from './ChevronRight.tsx'
+import ChevronUp from './ChevronUp.tsx'
 import Close from './Close.tsx'
+import Css from './Css.tsx'
+import DB from './DB.tsx'
+import Discord from './Discord.tsx'
+import Docker from './Docker.tsx'
+import ExpressJs from './ExpressJs.tsx'
+import Facebook from './Facebook.tsx'
+import Figma from './Figma.tsx'
+import Git from './Git.tsx'
+import Github from './Github.tsx'
+import Gitlab from './Gitlab.tsx'
+import Globe from './Globe.tsx'
+import Hammer from './Hammer.tsx'
+import Html from './Html.tsx'
+import Instagram from './Instagram.tsx'
+import Javascript from './Javascript.tsx'
+import Jira from './Jira.tsx'
+import LinkedIn from './LinkedIn.tsx'
+import Linux from './Linux.tsx'
+import List from './List.tsx'
+import Loading from './Loading.tsx'
+import Menu from './Menu.tsx'
+import Message from './Message.tsx';
+import MongoDB from './MongoDB.tsx'
+import Moon from './Moon.tsx'
+import NextJs from './NextJs.tsx'
+import NodeJs from './NodeJs.tsx'
+import Npm from './Npm.tsx'
+import Pin from './Pin.tsx'
+import PostgreSql from './PostgreSql.tsx'
+import Quotes from './Quotes.tsx'
 import React from './React.tsx'
-import Webstorm from './Webstorm.tsx';
-import Yarn from './Yarn.tsx';
+import Redux from './Redux.tsx'
+import Sass from './Sass.tsx'
+import Spinner from './Spinner.tsx'
+import Star from './Star.tsx'
+import StencilJs from './StencilJs.tsx'
+import Sun from './Sun.tsx'
+import Tailwindcss from './Tailwindcss.tsx'
+import Typescript from './Typescript.tsx'
+import User from './User.tsx'
+import Webstorm from './Webstorm.tsx'
+import Yarn from './Yarn.tsx'
 
 export default {
   Spinner,
@@ -85,5 +94,14 @@ export default {
   Tailwindcss,
   Typescript,
   Webstorm,
-  Yarn
+  Yarn,
+  ChevronDown,
+  ChevronUp,
+  ChevronLeft,
+  ChevronRight,
+  ArrowUp,
+  ArrowDown,
+  ArrowRight,
+  ArrowLeft,
+  Message,
 }
