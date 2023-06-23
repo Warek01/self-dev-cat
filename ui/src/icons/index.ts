48355e3401ee4db83a694c2aedee40ce76093d47
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2023-06-23 15:57:22 +0300

    feat: add more icons

--- ui/src/icons/index.ts
+++ ui/src/icons/index.ts
@@ -3,6 +3,7 @@ import ArrowLeft from './ArrowLeft.tsx'
 import ArrowRight from './ArrowRight.tsx'
 import ArrowUp from './ArrowUp.tsx'
 import Bash from './Bash.tsx'
+import Calendar from './Calendar.tsx'
 import ChevronDown from './ChevronDown.tsx'
 import ChevronLeft from './ChevronLeft.tsx'
 import ChevronRight from './ChevronRight.tsx'
@@ -12,9 +13,12 @@ import Css from './Css.tsx'
 import DB from './DB.tsx'
 import Discord from './Discord.tsx'
 import Docker from './Docker.tsx'
+import Download from './Download.tsx'
+import Eraser from './Eraser.tsx'
 import ExpressJs from './ExpressJs.tsx'
 import Facebook from './Facebook.tsx'
 import Figma from './Figma.tsx'
+import File from './File.tsx'
 import Git from './Git.tsx'
 import Github from './Github.tsx'
 import Gitlab from './Gitlab.tsx'
@@ -24,29 +28,36 @@ import Html from './Html.tsx'
 import Instagram from './Instagram.tsx'
 import Javascript from './Javascript.tsx'
 import Jira from './Jira.tsx'
+import Lightbulb from './Lightbulb.tsx'
 import LinkedIn from './LinkedIn.tsx'
 import Linux from './Linux.tsx'
 import List from './List.tsx'
 import Loading from './Loading.tsx'
+import Login from './Login.tsx'
+import Logout from './Logout.tsx'
 import Menu from './Menu.tsx'
-import Message from './Message.tsx';
+import Message from './Message.tsx'
 import MongoDB from './MongoDB.tsx'
 import Moon from './Moon.tsx'
 import NextJs from './NextJs.tsx'
 import NodeJs from './NodeJs.tsx'
 import Npm from './Npm.tsx'
+import Pen from './Pen.tsx'
 import Pin from './Pin.tsx'
 import PostgreSql from './PostgreSql.tsx'
 import Quotes from './Quotes.tsx'
 import React from './React.tsx'
 import Redux from './Redux.tsx'
 import Sass from './Sass.tsx'
+import Send from './Send.tsx'
+import Settings from './Settings.tsx'
 import Spinner from './Spinner.tsx'
 import Star from './Star.tsx'
 import StencilJs from './StencilJs.tsx'
 import Sun from './Sun.tsx'
 import Tailwindcss from './Tailwindcss.tsx'
 import Typescript from './Typescript.tsx'
+import Upload from './Upload.tsx'
 import User from './User.tsx'
 import Webstorm from './Webstorm.tsx'
 import Yarn from './Yarn.tsx'
@@ -104,4 +115,15 @@ export default {
   ArrowRight,
   ArrowLeft,
   Message,
+  Pen,
+  Eraser,
+  Settings,
+  Login,
+  Logout,
+  Send,
+  Upload,
+  Calendar,
+  File,
+  Lightbulb,
+  Download,
 }
